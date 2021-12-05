d512ad36d66e218420a3a6e6fcf3ead1fe5220fe
Danang <daffodil1411@gmail.com>
2021-12-05 11:25:14 +0700

    initial commit

---
+++ src/main/kotlin/com/jasavast/kotlinspringboilerplate/KotlinSpringBoilerplateApplication.kt
@@ -0,0 +1,11 @@
+package com.jasavast.kotlinspringboilerplate
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinSpringBoilerplateApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinSpringBoilerplateApplication>(*args)
+}
---
+++ src/test/kotlin/com/jasavast/kotlinspringboilerplate/KotlinSpringBoilerplateApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.jasavast.kotlinspringboilerplate
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinSpringBoilerplateApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
